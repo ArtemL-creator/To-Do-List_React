28a50b36c8dfcac60d4f7e76e9fe84811695cd49
misha <wolfhe4rt@yandex.ru>
2023-04-03 15:14:58 +0300

    Добавлены современные SVG

--- src/LeftColumnElem.js
+++ src/LeftColumnElem.js
@@ -5,25 +5,11 @@ export class LeftColumnElem extends React.Component {
   render() {
     const name = this.props.name;
     const count = this.props.count;
+    const svg = this.props.svg;
     return (
       <div className="leftColumnElem">
         <div className="leftColumnElemIcon">
-          <svg
-            width="30"
-            height="20"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect
-              x="10"
-              y="0"
-              width="20"
-              height="20"
-              stroke="black"
-              fill="transparent"
-              strokeWidth="5"
-            />
-          </svg>
+          {svg}
         </div>
 
         <div className="leftColumnElemText">{name}</div>
